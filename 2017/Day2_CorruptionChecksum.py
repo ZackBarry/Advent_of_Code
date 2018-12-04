eac0bcc3d5492d99e5794236ce3f8e5924c97663
Zachary <zackbarry13@gmail.com>
2018-12-03 20:07:55 -0800

    fixed a typo

--- 2017/Day2_CorruptionChecksum.py
+++ 2017/Day2_CorruptionChecksum.py
@@ -34,7 +34,7 @@ for row in rows:
 print('The checksum is ' + str(checksum) + '.')
 
 # -------------------------------------------------------------------------
-# Task 1: It sounds like the goal is to find the only two numbers in each
+# Task 2: It sounds like the goal is to find the only two numbers in each
 #         row where one evenly divides the other - that is, where the
 #         result of the division operation is a whole number. They would
 #         like you to find those numbers on each line, divide them, and add
